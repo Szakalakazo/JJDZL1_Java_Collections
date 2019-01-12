d401c3e6a65f1d35cf700f0b1e967fadca5029e8
Szakalakazo <tomek.starzak@gmail.com>
2019-01-12 16:39:39 +0100

    Java collections - ex 3 completed

--- src/main/java/com/infoshare/academy/data/structures/lists/ListTask.java
+++ src/main/java/com/infoshare/academy/data/structures/lists/ListTask.java
@@ -1,8 +1,44 @@
 package com.infoshare.academy.data.structures.lists;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class Person {
+    Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    private String name, surname;
+}
+
 public class ListTask {
 
     public static void main(String[] args) {
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("John", "Smith"));
+        list.add(new Person("Susan", "Kowalsky"));
+        list.add(new Person("Bob", "Rulofs"));
+
+        for (Person p : list) {
+            System.out.println(p.getName() + " " + p.getSurname());
+        }
 
     }
 }

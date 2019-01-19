b4f99b05c22c551e8bea834f5da943385f62d098
Szakalakazo <tomek.starzak@gmail.com>
2019-01-19 17:34:12 +0100

    HashMap example completed

--- src/main/java/com/infoshare/academy/data/structures/maps/HashMapTask.java
+++ src/main/java/com/infoshare/academy/data/structures/maps/HashMapTask.java
@@ -1,8 +1,30 @@
 package com.infoshare.academy.data.structures.maps;
 
+import com.infoshare.academy.data.structures.lists.Person;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class HashMapTask {
 
     public static void main(String[] args) {
 
+        Map<String, Person> map = new HashMap<>();
+        map.put("login3", new Person("Joe", "Ali"));
+        map.put("login2", new Person("Sue", "Cal"));
+        map.put("login1", new Person("Bob", "Brink"));
+        map.put("login4", new Person("Mike", "Sali"));
+
+
+
+        System.out.println("");
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+
+
+        System.out.println(map.get("login1"));
+        Set set = map.entrySet();
     }
 }

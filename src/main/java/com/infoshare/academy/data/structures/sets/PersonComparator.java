6a59129c792912aa94ffcce76fde91ffd183204e
Szakalakazo <tomek.starzak@gmail.com>
2019-01-19 16:28:19 +0100

    TreeSet and LinkedHashSet exercise completed

---
+++ src/main/java/com/infoshare/academy/data/structures/sets/PersonComparator.java
@@ -0,0 +1,10 @@
+package com.infoshare.academy.data.structures.sets;
+
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+}
--- src/main/java/com/infoshare/academy/data/structures/sets/SetTask.java
+++ src/main/java/com/infoshare/academy/data/structures/sets/SetTask.java
@@ -1,11 +1,9 @@
 package com.infoshare.academy.data.structures.sets;
 
 
-
 import com.infoshare.academy.data.structures.lists.Person;
 
-import java.util.HashSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SetTask {
 
@@ -17,10 +15,28 @@ public class SetTask {
         hashSet.add(new Person("Zbyszek", "Moniek"));
         hashSet.add(new Person("Tomek", "Bodzek"));
 
-        System.out.println(hashSet.toString());
 
-        TreeSet<Person> treeSet = new TreeSet<>(hashSet);
-        System.out.println(treeSet.toString());
+        TreeSet<Person> person = new TreeSet<>(hashSet);
+
+        System.out.println("\n-- treeSet person: ");
+        TreeSet<Person> p = new TreeSet<>();
+
+        PersonComparator personComparator = new PersonComparator();
+        Comparator<Person> reversed = personComparator.reversed();
+
+        person.add(new Person("Jan", "Nowak"));
+        person.add(new Person("Tomcio", "Paluch"));
+
+
+        System.out.println("-- linkedHashSet test: ");
+
+        Set<String> string = new LinkedHashSet<String>();
+
+        string.add("zwierzaki");
+        string.add("słodziaki");
+        string.add("alkoholicy");
+
+        System.out.println(string.toString());
 
 
     }

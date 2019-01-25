3a52dce9e013c3d36dd31824e38c8568e8f86ca7
Szakalakazo <tomek.starzak@gmail.com>
2019-01-25 09:14:38 +0100

    Fix logical errors, new conditions checks added.

--- src/main/java/com/infoshare/academy/data/structures/homework/Exercise21.java
+++ src/main/java/com/infoshare/academy/data/structures/homework/Exercise21.java
@@ -4,11 +4,13 @@ import java.util.*;
 
 public class Exercise21 {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new LinkedList<>();
         list.add(54);
         list.add(53);
-        list.add(2);
         list.add(12);
+//        list.add(176);
+//        list.add(176);
+        list.add(2);
         list.add(176);
         list.add(1);
 
@@ -16,20 +18,29 @@ public class Exercise21 {
 
         Integer max = Collections.max(list);
 
-        if (max.equals(list.get(list.size()-1))) {
+        /* Checking number of occurrence of max value in list. If more than once remove all duplicated vales*/
+        if (Collections.frequency(list, max) > 1) {
+            for (Iterator iterator = list.iterator(); iterator.hasNext(); ) {
+                Integer value = (Integer) iterator.next();
+                if (value.equals(max)) {
+                    iterator.remove();
+                }
+            }
+        }
+        /* Checking whether max value occurs only once and is at the end of list. If so leave list as it is */
+        if (max.equals(list.get(list.size() - 1))) {
             System.out.println("\nModified list: \n" + list);
             return;
         }
 
         list.add(max);
 
-        for (int i = 0; i < list.size() - 2; i++) {
+        for (int i = 0; i < list.size() - 1; i++) {
             if (list.get(i).equals(max)) {
                 list.remove(list.get(i));
             }
         }
 
         System.out.println("\nModified list: \n" + list);
-
     }
 }
